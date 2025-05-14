80569cc1bd632f9392573945bf202cf9a7156cb6
Caio Aires <aires@expected-it.com>
2025-05-14 14:38:18 +0200

    Added timing report generation pre and post timing repair in synth_repair.tcl

--- flow/scripts/synth_repair.tcl
+++ flow/scripts/synth_repair.tcl
@@ -1,7 +1,12 @@
 source $::env(SCRIPTS_DIR)/load.tcl
 source $::env(SCRIPTS_DIR)/util.tcl
 load_design 1_synth.v 1_synth.sdc
+
+report_check_types -violators -verbose >> $::env(REPORTS_DIR)/timing_pre_synth_repair.rpt
+
 repair_timing_helper 0
 write_verilog $::env(RESULTS_DIR)/1_2_synth-repair.v
 write_db $::env(RESULTS_DIR)/1_2_synth-repair.odb
 write_sdc -no_timestamp $::env(RESULTS_DIR)/1_2_synth-repair.sdc
+
+report_check_types -violators -verbose >> $::env(REPORTS_DIR)/timing_post_synth_repair.rpt
