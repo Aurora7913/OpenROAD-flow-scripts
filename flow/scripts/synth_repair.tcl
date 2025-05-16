5ba6fea9823f63f95075daad5edc553c3eda0c25
Dennis Gnad <dennis.gnad@expected-it.com>
2025-05-16 10:56:44 +0200

    Call the repair_timing_helper twice and also make the difference visible with two timing reports
    That was identified to make the only real difference. An exploration was done on PcDir that checks the difference in timing from varying all the possible parameters that we can give repair_timing if we call it directly. Only calling it two times could lead to an improvement, more than two times did also not make an improvement anymore.

--- flow/scripts/synth_repair.tcl
+++ flow/scripts/synth_repair.tcl
@@ -5,8 +5,13 @@ load_design 1_synth.v 1_synth.sdc
 report_check_types -violators -verbose >> $::env(REPORTS_DIR)/timing_pre_synth_repair.rpt
 
 repair_timing_helper 0
+
+report_check_types -violators -verbose >> $::env(REPORTS_DIR)/timing_post_synth_repair.rpt
+
+repair_timing_helper 0
+
+report_check_types -violators -verbose >> $::env(REPORTS_DIR)/timing_post_synth_repair_2.rpt
+
 write_verilog $::env(RESULTS_DIR)/1_2_synth-repair.v
 write_db $::env(RESULTS_DIR)/1_2_synth-repair.odb
 write_sdc -no_timestamp $::env(RESULTS_DIR)/1_2_synth-repair.sdc
-
-report_check_types -violators -verbose >> $::env(REPORTS_DIR)/timing_post_synth_repair.rpt
