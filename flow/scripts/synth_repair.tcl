f394c8ef58a1f294afbd7587de641f7f255193b6
Max Bjurling <max.bjurling@expected-it.com>
2026-02-24 16:35:17 +0100

    Use environment variable to determine the number of calls to `repair_timing`

--- flow/scripts/synth_repair.tcl
+++ flow/scripts/synth_repair.tcl
@@ -4,11 +4,14 @@ load_design 1_synth.v 1_synth.sdc
 
 report_check_types -violators -verbose > $::env(REPORTS_DIR)/1_1_Pre_synthesis_repair_detailed.rpt
 
-repair_timing_helper 0
-
-report_check_types -violators -verbose > $::env(REPORTS_DIR)/1_1_Post_synthesis_repair_detailed.rpt
-
-repair_timing_helper 0
+if {[info exists ::env(SYNTH_REPAIR_TIMING_LOOP_CNT)]} {
+    for {set i 0} {$i < $::env(SYNTH_REPAIR_TIMING_LOOP_CNT)} {incr i} {
+        puts "Running repair_timing, iteration [expr {$i + 1}] of $::env(SYNTH_REPAIR_TIMING_LOOP_CNT) ..."
+        repair_timing_helper 0
+    }
+} else {
+    puts "Warning: SYNTH_REPAIR_TIMING_LOOP_CNT environment variable not set, skipping repair_timing"
+}
 
 report_check_types -violators -verbose > $::env(REPORTS_DIR)/1_2_Post_synthesis_repair_detailed.rpt
 
